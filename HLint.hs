f2e5b2a085d8f0b177932e87ea01c53b35cfe359
David Luposchainsky <dluposchainsky@gmail.com>
2016-04-12 18:48:14 +0200

    Add HLint.hs

---
+++ HLint.hs
@@ -0,0 +1,14 @@
+-- Naming can be useful
+ignore "Avoid Lambda"
+ignore "Eta reduce"
+
+-- AMP fallout
+error "generalize mapM"  = mapM  ==> traverse
+error "generalize mapM_" = mapM_ ==> traverse_
+error "generalize forM"  = forM  ==> for
+error "generalize forM_" = forM_ ==> for_
+error "Avoid return" =
+    return ==> pure
+    where note = "return is obsolete as of GHC 7.10"
+
+error "Use parentheses instead of ($)" = f $ x ==> ()
