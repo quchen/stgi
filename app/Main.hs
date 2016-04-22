fb8c4067fe3505ac3f490ea57447db8b9c50e664
David Luposchainsky <dluposchainsky@gmail.com>
2016-04-22 17:51:39 +0200

    Change main to a very efficient infinite loop ;-)

--- app/Main.hs
+++ app/Main.hs
@@ -14,7 +14,6 @@ import qualified Data.Text.IO             as T
 import           System.Console.ANSI      (hSupportsANSI)
 import           System.IO                (stdout)
 
-import qualified Stg.Language.Prelude     as Stg
 import           Stg.Language.Prettyprint
 import           Stg.Machine
 import           Stg.Machine.Types
@@ -25,27 +24,10 @@ import           Stg.Util
 
 main :: IO ()
 main = do
-    let prog = Stg.foldl' <> Stg.add <> Stg.listC <> [stgProgram|
-        zero = () \n () -> Int# (0#);
-
-        sum = () \n (xs) -> foldl' (add, zero, xs);
-
-        list = () \u () ->
-            letrec one   = () \n () -> Int# (1#);
-                   two   = () \n () -> Int# (2#);
-                   three = () \n () -> Int# (3#);
-                   list3    = (three)          \n () -> cons (three, nil);
-                   list23   = (two, list3)     \n () -> cons (two,   list3);
-                   list123  = (one, list23)    \n () -> cons (one,   list23);
-                   list3123 = (three, list123) \n () -> cons (three, list123)
-            in list3123 ();
-
-        main = () \u () -> case sum (list) of
-            Int# (i) -> case i () of
-                9# -> Success ();
-                wrongResult -> TestFail (wrongResult);
-            default -> Error ()
+    let prog = [stgProgram|
+        main = () \n () -> main ()
         |]
+
         initial = initialState "main" prog
     ansiSupport <- hSupportsANSI stdout
     if ansiSupport || True
