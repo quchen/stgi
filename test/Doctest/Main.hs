8b38ab8d2de5963a1b317e3fcbd96e92bb438bcf
David Luposchainsky <dluposchainsky@gmail.com>
2016-05-03 17:39:41 +0200

    Don't run doctests when there are CMD args

--- test/Doctest/Main.hs
+++ test/Doctest/Main.hs
@@ -1,10 +1,14 @@
 module Main (main) where
 
 
+import           Control.Monad
+import           System.Environment
 
 import           Test.DocTest
 
 
 
 main :: IO ()
-main = doctest ["src"]
+main = do
+    args <- getArgs
+    when (null args) (doctest ["src"])
