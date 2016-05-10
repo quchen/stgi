5d2f0df81b34163f377d67e64f4b12f07d098180
David Luposchainsky <dluposchainsky@gmail.com>
2016-05-10 17:11:02 +0200

    Run doctests only if the 'doctest' Cabal flag is set

--- test/Doctest/Main.hs
+++ test/Doctest/Main.hs
@@ -1,14 +1,10 @@
 module Main (main) where
 
 
-import           Control.Monad
-import           System.Environment
 
 import           Test.DocTest
 
 
 
 main :: IO ()
-main = do
-    args <- getArgs
-    when (null args) (doctest ["src"])
+main = doctest ["src"]
