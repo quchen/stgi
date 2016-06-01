a52ec51facd6e10d3c7ae6f9f46fffd56dbdf186
David Luposchainsky <dluposchainsky@gmail.com>
2016-06-01 19:48:15 +0200

    Add marshalling test for giant type; refactor the test a bit

--- test/Testsuite/Test/Marshal.hs
+++ test/Testsuite/Test/Marshal.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NumDecimals         #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE QuasiQuotes         #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -10,7 +11,6 @@ import           Data.Proxy
 import           Data.Text  (Text)
 import qualified Data.Text  as T
 
-import Stg.Language
 import Stg.Language.Prettyprint
 import Stg.Machine
 import Stg.Marshal
@@ -30,21 +30,29 @@ tests = testGroup "Marshalling"
     , maybeRoundtrip
     , nestedMaybeRoundtrip
     , eitherRoundtrip
-    , tuple2Roundtrip
-    , tuple3Roundtrip
-    , tuple4Roundtrip
-    , tuple5Roundtrip ]
+    , testGroup "Tuples"
+        [tuple2Roundtrip
+        , tuple3Roundtrip
+        , tuple4Roundtrip
+        , tuple5Roundtrip ]
+    , crazyRoundtrip
+    ]
 
 -- | Specifies a test that is based on the reduction of a closure.
 data RoundtripSpec a = RoundtripSpec
     { testName :: Text
 
-    , force    :: Program
-        -- ^ @force@ function to evaluate the payload to WHNF
-
     , testType :: Proxy a
         -- ^ Pin down the type of @a@
+
+    , maxSteps :: Integer
     }
+
+defSpec :: RoundtripSpec a
+defSpec = RoundtripSpec
+    { testName = "Default test spec"
+    , testType = Proxy
+    , maxSteps = 10e3}
 
 roundTripTest
     :: forall a. (Arbitrary a, ToStg a, FromStg a, Show a, Eq a)
@@ -53,114 +61,112 @@ roundTripTest
 roundTripTest spec = testProperty (T.unpack (testName spec)) test
   where
     test :: (Arbitrary a, ToStg a, FromStg a, Show a, Eq a) => a -> Property
-    test payload = counterexample (T.unpack (prettyprint source)) (expected === Right payload)
+    test payload = counterexample (T.unpack (prettyprint finalState))
+                                  (expected === Right payload)
       where
         source = mconcat
-            [ force spec
-            , toStg "payload" payload
+            [ toStg "payload" payload
             , [stg|
-                main = \ => force payload |]]
+                main = \ => case forceUntyped payload of _ -> Done;
+
+                forceUntyped = \value -> case value of
+                    Unit -> Done;
+
+                    Nothing -> Done;
+                    Just x -> forceUntyped x;
+
+                    True -> Done;
+                    False -> Done;
+
+                    Int# _ -> Done;
+
+                    Left l -> forceUntyped l;
+                    Right r -> forceUntyped r;
+
+                    Pair x y -> case forceUntyped x of
+                        _ -> forceUntyped y;
+                    Triple x y z -> case forceUntyped x of
+                        _ -> case forceUntyped y of
+                            _ -> forceUntyped z;
+
+                    Nil -> Done;
+                    Cons x xs -> case forceUntyped x of
+                        _ -> forceUntyped xs;
+
+                    _ -> Error
+                 |]]
         prog = initialState "main" source
         states = evalsUntil
-            (RunForMaxSteps 1000)
+            (RunForMaxSteps (maxSteps spec))
             (HaltIf (const False))
             (PerformGc (const Nothing))
             prog
-        finalState = last states
-        expected = fromStg finalState "main"
+        finalState = garbageCollect triStateTracing (last states)
+        expected = fromStg finalState "payload"
 
 
 boolRoundtrip :: TestTree
-boolRoundtrip = roundTripTest RoundtripSpec
+boolRoundtrip = roundTripTest defSpec
     { testName = "Bool"
-    , testType = Proxy :: Proxy Bool
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy Bool }
 
 integerRoundtrip :: TestTree
-integerRoundtrip = roundTripTest RoundtripSpec
+integerRoundtrip = roundTripTest defSpec
     { testName = "Integer"
-    , testType = Proxy :: Proxy Integer
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy Integer }
 
 unitRoundtrip :: TestTree
-unitRoundtrip = roundTripTest RoundtripSpec
+unitRoundtrip = roundTripTest defSpec
     { testName = "Unit"
-    , testType = Proxy :: Proxy ()
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy () }
 
 listRoundtrip :: TestTree
-listRoundtrip = roundTripTest RoundtripSpec
+listRoundtrip = roundTripTest defSpec
     { testName = "List"
-    , testType = Proxy :: Proxy [Integer]
-    , force = [stg|
-        force = \xs -> case forceList xs of _ -> xs;
-        forceList = \xs -> case xs of
-            Nil -> Done;
-            Cons y ys -> case y of
-                _ -> forceList ys;
-            _ -> Error
-        |] }
+    , testType = Proxy :: Proxy [Integer] }
 
 nestedListRoundtrip :: TestTree
-nestedListRoundtrip = roundTripTest RoundtripSpec
+nestedListRoundtrip = roundTripTest defSpec
     { testName = "Nested list"
-    , testType = Proxy :: Proxy [[Integer]]
-    , force = [stg|
-        force = \x -> case forceUntyped x of _ -> x;
-
-        forceUntyped = \x -> case x of
-            Nil -> Done;
-            Int# _ -> Done;
-            Cons ys yss -> case forceUntyped ys of
-                _ -> forceUntyped yss;
-            _ -> Error
-        |] }
+    , testType = Proxy :: Proxy [[Integer]] }
 
 maybeRoundtrip :: TestTree
-maybeRoundtrip = roundTripTest RoundtripSpec
+maybeRoundtrip = roundTripTest defSpec
     { testName = "Maybe"
-    , testType = Proxy :: Proxy (Maybe Integer)
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy (Maybe Integer) }
 
 nestedMaybeRoundtrip :: TestTree
-nestedMaybeRoundtrip = roundTripTest RoundtripSpec
+nestedMaybeRoundtrip = roundTripTest defSpec
     { testName = "Maybe"
-    , testType = Proxy :: Proxy (Maybe (Maybe Integer))
-    , force = [stg|
-        force = \x -> case forceUntyped x of _ -> x;
-
-        forceUntyped = \x -> case x of
-            Nothing -> Done;
-            Int# _ -> Done;
-            Just y -> forceUntyped y;
-            _ -> Error |] }
+    , testType = Proxy :: Proxy (Maybe (Maybe Integer)) }
 
 eitherRoundtrip :: TestTree
-eitherRoundtrip = roundTripTest RoundtripSpec
+eitherRoundtrip = roundTripTest defSpec
     { testName = "Maybe"
-    , testType = Proxy :: Proxy (Either Integer Bool)
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy (Either Integer Bool) }
 
 tuple2Roundtrip :: TestTree
-tuple2Roundtrip = roundTripTest RoundtripSpec
+tuple2Roundtrip = roundTripTest defSpec
     { testName = "2-tuple"
-    , testType = Proxy :: Proxy (Integer, Integer)
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy (Integer, Integer) }
 
 tuple3Roundtrip :: TestTree
-tuple3Roundtrip = roundTripTest RoundtripSpec
+tuple3Roundtrip = roundTripTest defSpec
     { testName = "3-tuple"
-    , testType = Proxy :: Proxy (Integer, Integer, Integer)
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy (Integer, Integer, Integer) }
 
 tuple4Roundtrip :: TestTree
-tuple4Roundtrip = roundTripTest RoundtripSpec
+tuple4Roundtrip = roundTripTest defSpec
     { testName = "4-tuple"
-    , testType = Proxy :: Proxy (Integer, Integer, Integer, Integer)
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy (Integer, Integer, Integer, Integer) }
 
 tuple5Roundtrip :: TestTree
-tuple5Roundtrip = roundTripTest RoundtripSpec
+tuple5Roundtrip = roundTripTest defSpec
     { testName = "5-tuple"
-    , testType = Proxy :: Proxy (Integer, Integer, Integer, Integer, Integer)
-    , force = [stg| force = \x -> case x of r -> r |] }
+    , testType = Proxy :: Proxy (Integer, Integer, Integer, Integer, Integer) }
+
+crazyRoundtrip :: TestTree
+crazyRoundtrip = roundTripTest defSpec
+    { testName = "Crazy giant type"
+    , testType = Proxy :: Proxy [Either ([[[Integer]]], [Maybe ()]) (Maybe [([Bool], Integer, [Bool])])]
+    , maxSteps = 10e5 }
